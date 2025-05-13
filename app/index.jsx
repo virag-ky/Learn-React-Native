18e79a8a11a7fba23cadac44759a7ad6d8cdfc93
virag-ky <vkormoczy@gmail.com>
2025-05-13 16:07:46 +0300

    Add logo and basic styles

---
+++ app/index.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from "../assets/img/logo_light.png";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={styles.title}>The number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    marginVertical: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
