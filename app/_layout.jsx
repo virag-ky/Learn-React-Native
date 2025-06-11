4ff0bca0d6282851d8886e950ab0189afc632d88
virag-ky <vkormoczy@gmail.com>
2025-06-11 16:44:28 +0300

    Add dashboard route group

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -16,8 +16,9 @@ const RootLayout = () => {
           headerTintColor: theme.title,
         }}
       >
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
--- app/index.jsx
+++ app/index.jsx
@@ -22,6 +22,9 @@ const Home = () => {
       <Link href="/register" style={styles.link}>
         <ThemedText>Register Page</ThemedText>
       </Link>
+      <Link href="/profile" style={styles.link}>
+        <ThemedText>Profile Page</ThemedText>
+      </Link>
     </ThemedView>
   );
 };
